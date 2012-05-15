b80150ece99ae420c2094fa3836204dbc4148702
Ale-x-ei <neznaika_50@mail.ru>
2012-05-15 21:29:44 +0300

    Signed-off-by: Ale-x-ei <neznaika_50@mail.ru>

---
+++ modules/galerie/galerie/galerie-thumbnail.tpl.php
@@ -0,0 +1,8 @@
+<a href="<?php echo $image['thumb-link'] ?>"><img
+  title="<?php echo $image['title'] ?>"
+  alt="<?php echo ($image['title'] ? $image['title'] : $image['id']) ?>"
+  id="galerie-<?php echo $image['id'] ?>"
+  src="<?php echo $image['thumb-src'] ?>"
+  height="75"
+  width="75"
+/></a>
---
+++ modules/galerie/galerie/galerie.tpl.php
@@ -0,0 +1,15 @@
+<div class="galerie-wrapper galerie-<?php echo $galerie_type ?>">
+  <div class="galerie-browser-wrapper">
+    <div class="galerie-browser">
+      <?php echo $thumbnails ?>
+    </div>
+    <div class="galerie-browser-more">
+      <?php echo $more_link ?>
+    </div>
+  </div>
+  <div class="galerie-viewer-wrapper">
+    <div class="galerie-viewer">
+      <?php echo $image ?>
+    </div>
+  </div>
+</div>
